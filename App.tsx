aa0de767ac0e86b27c093acd2fd833a3bb4250ca
Danilo Henrique <daniloohenriquee@gmail.com>
2023-08-05 17:03:45 -0300

    Conclusão da pagina inicial

--- App.tsx
+++ App.tsx
@@ -1,19 +1,54 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import { ThemeProvider } from "styled-components";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from 'react-native';
+
+import {
+    useFonts,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+} from "@expo-google-fonts/poppins";
+
+import theme from "./src/global/styles/theme";
+import { Dashboard } from "./src/screens/Dashboard";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	const [isLoaded] = useFonts({
+		Poppins_400Regular,
+		Poppins_500Medium,
+		Poppins_700Bold
+	});
+
+	useEffect(() => {
+		async function showSplashScreen() {
+			await SplashScreen.preventAutoHideAsync();
+		}
+	
+		showSplashScreen();
+	}, []);
+
+	useEffect(() => {
+		const hideSplashScreen = async () => {
+			await SplashScreen.hideAsync();
+		};
+
+		if (isLoaded)
+			hideSplashScreen();
+
+	}, [isLoaded]);
+	
+	if (!isLoaded) return null;
+
+    return (
+        <ThemeProvider theme={theme}>
+			<StatusBar
+				backgroundColor="transparent"
+				translucent
+				barStyle="light-content"
+			/>
+            <Dashboard />
+        </ThemeProvider>
+    );
+}
